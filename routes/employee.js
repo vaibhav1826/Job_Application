87a617c7c654706ab533d3fde9ec0b6b40d28983
Vaibhav bhatt <148764896+vaibhav1826@users.noreply.github.com>
2024-08-04 01:03:36 +0530

    adding routes for employee

---
+++ routes/employee.js
@@ -0,0 +1,47 @@
+const {Router} = require('express');
+const multer =require('multer');
+const Employee = require('../models/employee');
+const path = require('path');
+const route = Router();
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, path.resolve(`./public/uploads/`));
+    },
+    filename: function (req, file, cb) {
+      const fileName = `${Date.now()}-${file.originalname}`;
+      cb(null, fileName);
+    },
+  });
+
+  const upload = multer({ storage: storage });
+
+route.get("/add-new",(req,res)=>{
+    return res.render('addemployees',{
+        user : req.user,
+    });
+});
+
+route.get("/:id", async (req, res) => {
+  const employee = await Employee.findById(req.params.id).populate("createdBy");
+  return res.render("employee", {
+    user: req.user,
+    employee,
+  });
+});
+
+
+route.post("/",upload.single("coverImage"),async(req,res)=>{
+    const { title, body } = req.body;
+    const employee = await Employee.create({
+      body,
+      title,
+      createdBy: req.user._id,
+      coverImageURL: `/uploads/${req.file.filename}`,
+    });
+    return res.redirect(`/employee/${employee._id}`);
+  });
+  
+
+
+module.exports = route;
