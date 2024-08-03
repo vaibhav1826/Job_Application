224ab9632e4107e422f5d2b6bc34653834eee1bb
Vaibhav bhatt <148764896+vaibhav1826@users.noreply.github.com>
2024-08-03 13:16:01 +0530

    installing mongoose

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+const Port = 1800;
+
+
+
+//server starting at localhost:1800
+app.listen(Port,()=>{
+    console.log(`server at localhost:${Port}`);
+})
